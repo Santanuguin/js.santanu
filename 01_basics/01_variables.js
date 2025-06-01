844de0b44250faef8cd80c4c280cf4292b6fbaef
Santanu Guin <146315529+Santanuguin@users.noreply.github.com>
2025-06-01 13:02:17 +0000

    add datatypes learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "santanu@google.com"
+var accountPassword= "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword="21212121"
+accountCity="Bangalore"
+
+console.log(accountId);
+
+console.table([accountId ,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3+3)
+
+console.log("Santanu")
+
+let name = "Santanu"
+let age = 21
+let isLoggedIn = false
+
+
+console.log(typeof null);//object
+
+
+
