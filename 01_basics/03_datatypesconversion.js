55484ccec3a6281319e9118f2277709272c5a810
Santanu Guin <146315529+Santanuguin@users.noreply.github.com>
2025-06-01 14:49:07 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatypesconversion.js
@@ -0,0 +1,33 @@
+let score = "Santanu"
+
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+ let isLoggedIn = ""
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+ //console.log(booleanIsLoggedIn);
+
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ //console.log(stringNumber);
+
+ //console.log(typeof stringNumber);
+
+ let somestring = "Santanu"
+
+ let strNum = Number(somestring)
+ console.log(strNum);
+ console.log(typeof strNum);
+ 
+ 
+ 
+ 
+
+ 
